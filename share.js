f40b2eb1dada6d6d44b483182920d68f16448010
Avery <averyking@sc.edu>
2025-10-26 21:49:25 -0400

    added share button the soil page

--- share.js
+++ share.js
@@ -1,21 +1,11 @@
 const currentPageUrl = encodeURIComponent(window.location.href); 
-const shareText = encodeURIComponent('Check out my website hosted on GitHub Pages!');
-
-// B. Function for Twitter
-function shareOnTwitter() {
-    // Twitter's share API: https://twitter.com/intent/tweet?text=[text]&url=[url]
-    const twitterUrl = `https://twitter.com/intent/tweet?text=${shareText}&url=${currentPageUrl}`;
-    
-   
-    window.open(twitterUrl, 'Share to Twitter', 'width=600,height=400');
+const shareText = encodeURIComponent('Check out this website hosted on GitHub.');
+function shareOnFacebook() {    
+    const FacebookUrl = `https://www.facebook.com/sharer/sharer.php?u=${currentPageUrl}`;  
+    window.open(FacebookUrl, 'Share to Facebook', 'width=600,height=400');
 }
-
-// C. Function for LinkedIn
 function shareOnLinkedIn() {
     const pageTitle = encodeURIComponent(document.title);
-
-    // LinkedIn's share API: https://www.linkedin.com/shareArticle?url=[url]&title=[title]
-    const linkedInUrl = `https://www.linkedin.com/shareArticle?url=${currentPageUrl}&title=${pageTitle}`;
-    
+    const linkedInUrl = `https://www.linkedin.com/shareArticle?url=${currentPageUrl}&title=${pageTitle}`;   
     window.open(linkedInUrl, 'Share to LinkedIn', 'width=600,height=400');
 }
